c5b0bc4ea05afd44f3a734bcea2cad4da5ff5abd
rabaahar <harfanerabaa@gmail.com>
2022-10-16 22:29:28 +0300

    last change check

---
+++ Check/CourseInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check
+{
+    public class CourseInfo
+    {
+        public int Id { get; set; }
+        public string Course_Name { get; set; }
+        public List<string> Students { get; set; } = new List<string>();
+        public string? Languge { get; set; }    
+
+        
+
+    }
+}
---
+++ Check/Rules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check
+{
+    public class Rules
+    {
+        public int Rule_Number { get; set; }
+        public int Points { get; set; }
+        public string ?File_Name { get; set; }
+        public string ?Regular_Exp { get; set; }    // regular expression
+
+        
+    }
+}
