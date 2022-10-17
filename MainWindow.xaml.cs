2124f2fc9ad1e17e515d7491d8c7534346c169ea
rabaahar <harfanerabaa@gmail.com>
2022-10-17 16:03:02 +0300

    changing the window frame

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace CSharp_FinalProject
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
