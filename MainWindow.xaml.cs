cb848e27cc8ea2f8e902616e2958161141d39344
rabaahar <harfanerabaa@gmail.com>
2022-10-19 10:53:24 +0300

    design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,13 @@ namespace CSharp_FinalProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        public readonly string[] options = { "Hi" , "Bye" , "Shahd" , "rabea", "rabaa" };
+
+
         public MainWindow()
         {
             InitializeComponent();
+            bindListBox();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -42,6 +46,18 @@ namespace CSharp_FinalProject
 
         private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
         {
+
+        }
+
+        private void bindListBox()
+        {
+            MyListBox.ItemsSource = options;
+        }
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //<TextBox HorizontalAlignment="Left" Height="25" Margin="600,100,0,0" TextWrapping="Wrap" Text="  Year " VerticalAlignment="Top" Width="130" FontSize="18" TextChanged="TextBox_TextChanged_2" Foreground="SeaGreen"/>
+            MessageBox.Show(MyListBox.SelectedItem.ToString());
+
 
         }
     }
